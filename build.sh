e03a5a7f7a7230afeba2674adfc85d22ce5176fe
Tom Brus <tombrus@gmail.com>
2018-11-14 17:17:06 +0100

    init

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -ue
 
 mavenRepoDir="$1"; shift
       jdkDir="$1"; shift
+  minVersion="$1"; shift
 
- MIN_VERSION=2.151
 DOWNLOAD_DIR=tmp-plugins
 
 export MAVEN_OPTS="-Dmaven.repo.local=$mavenRepoDir"
@@ -17,18 +17,20 @@ echo
 
 echo "======== build juseppe"
 (   cd juseppe
-    git checkout master
-    git clean -fdx
-    git pull
-     ./build.sh
+    (   git checkout master
+        git clean -fdx
+        git pull
+    ) > git.log
+    ./build.sh
 )
 echo
 
 echo "======== build update-center2"
 (   cd update-center2
-    git checkout master
-    git clean -fdx
-    git pull
+    (   git checkout master
+        git clean -fdx
+        git pull
+    ) > git.log
     ./build.sh
 )
 echo
@@ -37,5 +39,5 @@ echo "======== making download dir with plugins"
 java \
     -cp update-center2/target/update-center2-*-bin*/update-center2-*.jar \
     org.jvnet.hudson.update_center.MainOnlyDownload \
-    -version  "$MIN_VERSION" \
+    -version  "$minVersion" \
     -download "$DOWNLOAD_DIR"
