d04130081eeb117c8b39f1b1f5353bd69bc0c5a9
Tom Brus <tombrus@gmail.com>
2018-11-14 14:02:02 +0100

    init

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
+set -ue
 
+mavenRepoDir="$1"; shift
+      jdkDir="$1"; shift
+
+
+export MAVEN_OPTS="-Dmaven.repo.local=$mavenRepoDir"
 echo
 
+echo "======== setup submodules"
 git submodule init
 git submodule update
 echo
 
-(cd juseppe       ; git pull)
-(cd update-center2; git pull)
+echo "======== build juseppe"
+(   cd juseppe
+    git pull
+     ./build.sh
+)
 echo
 
-(cd juseppe       ; ./build.sh)
-(cd update-center2; ./build.sh)
+echo "======== build update-center2"
+(   cd update-center2
+    git pull
+    ./build.sh
+)
 echo
 
