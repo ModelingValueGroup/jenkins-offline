efe88a7e6c3f66db23709923a8dedb0186d95e49
Tom Brus <tombrus@gmail.com>
2018-11-14 13:47:19 +0100

    init

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 echo
+
 git submodule init
 git submodule update
+echo
+
 (cd juseppe       ; git pull)
 (cd update-center2; git pull)
 echo
+
+(cd juseppe       ; ./build.sh)
+(cd update-center2; ./build.sh)
+echo
+
